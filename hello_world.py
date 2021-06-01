f34865166bd013039369f18fdaad3c9126c92f9d
tenappdev <85103106+tenappdev@users.noreply.github.com>
2021-06-01 15:53:34 +0700

    Revert "Merge pull request #2 from tenappdev/third_branch"
    This reverts commit 24b5582a676dd170b862420c2d161fc04176de0f, reversing
    changes made to 526036d373cd004644d6b749b4951dd37c0a8e21.

--- hello_world.py
+++ hello_world.py
@@ -2,6 +2,3 @@ print('Learn Git Desktop')
 print("Source https://www.youtube.com/watch?v=fIAkdihd6Fg&t=124s&ab_channel=PatiphanPhengpao")
 
 print("This is TEXT from second branch")
-
-for i in range(2):
-    print("This is third branch",i)
